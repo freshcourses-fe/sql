b7a44a0ad4ded464241cbc4277d6b74c7e20c846
Kirill <freshjs.ftd.@gmail.com>
2022-06-09 19:06:08 +0300

    Add constraints examples

--- code.sql
+++ code.sql
@@ -3,19 +3,19 @@
 
 -- DDL
 CREATE TABLE users(
-  first_name varchar(64) NOT NULL,
-  last_name varchar(64) NOT NULL,
-  email varchar(100) NOT NULL,
-  height numeric(3, 2),
-  is_male bool,
-  birthday date
+  first_name varchar(64) NOT NULL CHECK (first_name != ''),
+  last_name varchar(64) NOT NULL CHECK (last_name != ''),
+  email varchar(100) NOT NULL UNIQUE CONSTRAINT "VALID EMAIL" CHECK (email ~ '^[A-Za-z0-9]\w{0,}@[a-z]{1,}\.[a-z]{1,}$'),
+  height numeric(3, 2) CHECK (height > 0 AND height < 3),
+  is_male bool NOT NULL,
+  birthday date NOT NULL CHECK (age(birthday) < interval '120 years' AND birthday < (current_date + interval '1 day'))
 );
 
 --DML
 INSERT INTO users
-VALUES (NULL, 'Testovich', 'test@test.test', 1.60, true, '1999-1-1');
+VALUES ('Test', 'Testovich', 'test23@test.test', 1.60, true, '2022-6-9');
 -- incomplete insert
-INSERT INTO users DEFAULT VALUES;
+INSERT INTO users VALUES ('Anton', 'test', 'anton@test.test', 2, true, '1923-1-1');
 
 DROP TABLE users;
 /*
