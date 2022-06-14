9874a3ce7031c65529e7c3b5eac80e8f87b5a83c
Kirill <freshjs.ftd.@gmail.com>
2022-06-14 19:47:16 +0300

    Add basic select examples

---
+++ dql.sql
@@ -0,0 +1,30 @@
+-- DQL - data query language
+
+SELECT * FROM users; -- все пользователи
+
+SELECT email FROM users; -- емейлы всех пользователей
+
+
+SELECT * FROM users
+WHERE id % 2 = 0 ; -- всех пользователей с четным айди
+
+SELECT * FROM users
+WHERE CAST(id^3 AS numeric) % 3 = 0 ; -- все юзеры, айдишники которых в кубе кратны трем
+
+SELECT id, first_name AS "Name", email FROM users AS u;
+SELECT id, first_name "Name", email FROM users u;
+--
+SELECT * FROM users
+WHERE 100 <= id AND id <= 105;
+
+SELECT * FROM users
+WHERE id BETWEEN 101 AND 105;
+
+SELECT * FROM users
+WHERE id BETWEEN SYMMETRIC 105 AND 101;
+
+SELECT * FROM users
+WHERE id IN (101,102,103,104,105);
+--
+SELECT * FROM users
+WHERE first_name IN ('John', 'Jane');
