67d9564463b5ea6b966c3bcc279b82547155abc5
Kirill <freshjs.ftd.@gmail.com>
2022-06-15 19:50:26 +0300

    Add solution

--- tasks/task.sql
+++ tasks/task.sql
@@ -1,1 +1,20 @@
+-- Найти рост самого высокого мужчины
+-- v1 aggregation
+SELECT max(height) FROM users
+WHERE is_male;
+-- v1.5
+SELECT max(height), is_male FROM users
+GROUP BY is_male;
+-- v2 SORT + LIMIT
+SELECT height
+FROM users
+WHERE is_male = true
+ORDER BY height DESC
+LIMIT 1;
 -- Найти самого высокого мужчину
+SELECT *
+FROM users
+WHERE is_male = true
+ORDER BY height DESC,
+birthday ASC
+LIMIT 10;
