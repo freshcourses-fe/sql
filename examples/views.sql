379f08fafa5bde7b472605486a2bcc3ea3d50ba8
Kirill <freshjs.ftd.@gmail.com>
2022-06-21 20:39:45 +0300

    Add explanation + task

--- examples/views.sql
+++ examples/views.sql
@@ -1,3 +1,4 @@
+-- Хранит запрос в БД и запускает его когда к ней стучатся
 CREATE VIEW orders_with_price AS 
 SELECT orders.id "Order id", users.id "User id", sum(price * pto.quantity) "Total price"
 FROM users
@@ -10,7 +11,7 @@ DROP VIEW orders_with_price;
 --
 -- Надо общую сумму покупок пользователей
 SELECT sum("Total price") "Total user spending" FROM orders_with_price;
--- MATERIALIZED
+-- MATERIALIZED - хранит результаты запроса в БД
 CREATE MATERIALIZED VIEW orders_with_price_m AS 
 SELECT orders.id "Order id", users.id "User id", sum(price * pto.quantity) "Total price"
 FROM users
@@ -19,6 +20,13 @@ JOIN products_to_orders pto ON pto.order_id = orders.id
 JOIN products ON products.id = pto.product_id
 GROUP BY orders.id, users.id
 WITH DATA; -- говорим что надо заполнить материализованные данные
+-- WITH NO DATA если сначала не хотм туда ложить данные
 
 REFRESH MATERIALIZED VIEW orders_with_price_m; -- пока вы обновляетесь то к таблицам не постучатся
 REFRESH MATERIALIZED VIEW CONCURRENTLY orders_with_price_m; -- а тут можно
+
+/*
+  Создайте мне вьюху в которой
+  Все данные про пользователей - продавцов + количество товаров этих продавцов
+  пол пользоввателя указать словами
+*/
