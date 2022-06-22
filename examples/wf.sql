a4424eb980b5df06b826921781c11799b800dceb
Kirill <freshjs.ftd.@gmail.com>
2022-06-22 19:10:12 +0300

    Add window functions

---
+++ examples/wf.sql
@@ -0,0 +1,107 @@
+SELECT
+  users.*
+FROM
+  users
+  JOIN orders ON orders.buyer_id = users.id
+GROUP BY
+  users.id;
+
+-- + сумма всех покупок
+SELECT
+  sum(products.price * pto.quantity) total_sum,
+  users.*
+FROM
+  users
+  JOIN orders ON orders.buyer_id = users.id
+  JOIN products_to_orders pto ON pto.order_id = orders.id
+  JOIN products ON products.id = pto.product_id
+GROUP BY
+  users.id;
+
+-- сколько в всреднем пользователи потратились
+SELECT
+  avg(results.total_sum)
+FROM (
+  SELECT
+    sum(products.price * pto.quantity) total_sum,
+    users.*
+  FROM
+    users
+    JOIN orders ON orders.buyer_id = users.id
+    JOIN products_to_orders pto ON pto.order_id = orders.id
+    JOIN products ON products.id = pto.product_id
+  GROUP BY
+    users.id) results;
+
+--
+SELECT
+  average.*,
+  sum(products.price * pto.quantity) total_sum,
+  users.*
+FROM
+  users
+  JOIN orders ON orders.buyer_id = users.id
+  JOIN products_to_orders pto ON pto.order_id = orders.id
+  JOIN products ON products.id = pto.product_id
+  CROSS JOIN (
+    SELECT
+      avg(results.total_sum) average_spent_on_orders
+    FROM (
+      SELECT
+        sum(products.price * pto.quantity) total_sum,
+        users.*
+      FROM
+        users
+        JOIN orders ON orders.buyer_id = users.id
+        JOIN products_to_orders pto ON pto.order_id = orders.id
+        JOIN products ON products.id = pto.product_id
+      GROUP BY
+        users.id) results) average
+GROUP BY
+  users.id,
+  average.average_spent_on_orders
+  /*
+   Оконные функции
+   */
+  SELECT
+    sum(products.price * pto.quantity) total_sum,
+  avg(sum(products.price * pto.quantity)) OVER () average_sum,
+    users.*
+  FROM
+    users
+    JOIN orders ON orders.buyer_id = users.id
+    JOIN products_to_orders pto ON pto.order_id = orders.id
+    JOIN products ON products.id = pto.product_id
+  GROUP BY
+    users.id;
+
+--
+SELECT
+  id,
+  "name",
+  category,
+  brand,
+  price,
+  quantity,
+  sum(price * quantity) OVER () total_price_of_all,
+  sum(price * quantity) OVER (PARTITION BY id) total_price-- замена GROUP BY
+FROM
+  products
+;
+
+SELECT
+  id,
+  "name",
+  category,
+  brand,
+  price,
+  quantity,
+  sum(price * quantity) OVER () total_price_of_all,
+  sum(price * quantity)
+FROM
+  products
+GROUP BY
+  id
+ORDER BY
+  id;
+
